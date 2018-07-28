5b5ea7487b2bc73edc1ed62bf6b4e52ab466abc2
gregoryjvrusso <gregoryjvrusso@gmail.com>
2018-07-28 20:05:32 -0300

    Testando envio de e-mail

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
-require_once("/php/cabecalho.php");
+require_once("php/cabecalho.php");
 //require_once("usuario-logica.php");
 
 
 
-require_once("/php/rodape.php"); ?>
+require_once("php/rodape.php"); ?>
 
 
