a7a0829c5a597049adcb2f7b3c2427c0d0c71e9c
gregoryjvrusso <gregoryjvrusso@gmail.com>
2018-07-28 19:57:08 -0300

    Testando envio de e-mail

--- aluno-certificado-palestra.php
+++ aluno-certificado-palestra.php
@@ -3,6 +3,7 @@ require_once("dompdf/dompdf_config.inc.php");
 require_once("banco-aluno.php");
 require_once("banco-palestra.php");
 require_once("usuario-logica.php");
+require_once("PHPMailer/src/PHPMailer.php");
 
 $aluno = buscaAlunoId($conexao, $_POST['id-aluno']);
 
@@ -94,4 +95,36 @@ $dompdf->stream(
     )
 );
 
+define('GUSER', 'techweekcubatao@gmail.com');	// <-- Insira aqui o seu GMail
+define('GPWD', 'Techweek2016');		// <-- Insira aqui a senha do seu GMail
 
+
+if (smtpmailer('gregoryjvrusso@gmail.com', 'techweekcubatao@gmail.com', 'Nome do Enviador', 'Assunto do Email', 'teste')) {
+
+	Header("location:http://www.dominio.com.br/obrigado.html"); // Redireciona para uma página de obrigado.
+}
+if (!empty($error)) echo $error;
+
+function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
+	global $error;
+	$mail = new PHPMailer();
+	$mail->IsSMTP();		// Ativar SMTP
+	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
+	$mail->SMTPAuth = true;		// Autenticação ativada
+	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
+	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
+	$mail->Port = 587;  		// A porta 587 deverá estar aberta em seu servidor
+	$mail->Username = GUSER;
+	$mail->Password = GPWD;
+	$mail->SetFrom($de, $de_nome);
+	$mail->Subject = $assunto;
+	$mail->Body = $corpo;
+	$mail->AddAddress($para);
+	if(!$mail->Send()) {
+		$error = 'Mail error: '.$mail->ErrorInfo; 
+		return false;
+	} else {
+		$error = 'Mensagem enviada!';
+		return true;
+	}
+}
