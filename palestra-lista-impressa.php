ad63adf1d9cbd6dfa8304eac085646606e55543b
gregoryjvrusso <gregoryjvrusso@gmail.com>
2018-07-28 20:46:07 -0300

    teste

--- palestra-lista-impressa.php
+++ palestra-lista-impressa.php
@@ -92,4 +92,36 @@ $dompdf->stream(
     )
 );
 
+define('GUSER', 'techweekcubatao@gmail.com');	// <-- Insira aqui o seu GMail
+define('GPWD', 'Techweek2016');		// <-- Insira aqui a senha do seu GMail
 
+function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
+	global $error;
+	$mail = new PHPMailer();
+	$mail->IsSMTP();		// Ativar SMTP
+	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
+	$mail->SMTPAuth = true;		// Autenticação ativada
+	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
+	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
+	$mail->Port = 587;  		// A porta 587 deverá estar aberta em seu servidor
+	$mail->Username = GUSER;
+	$mail->Password = GPWD;
+	$mail->SetFrom($de, $de_nome);
+	$mail->Subject = $assunto;
+	$mail->Body = $corpo;
+	$mail->AddAddress($para);
+	if(!$mail->Send()) {
+		$error = 'Mail error: '.$mail->ErrorInfo; 
+		return false;
+	} else {
+		$error = 'Mensagem enviada!';
+		return true;
+	}
+}
+
+if(smtpmailer('gregoryjvrusso@gmail.com', 'techweekcubatao@gmail.com', 'Nome do Enviador', 'Assunto do Email', "Teste")) {
+
+	Header("location:http://www.dominio.com.br/obrigado.html"); // Redireciona para uma página de obrigado.
+}
+if (!empty($error)) echo $error;
+?>
