5610ae7a1d79a6df063a450a24d8b53b245a62d9
gregoryjvrusso <gregoryjvrusso@gmail.com>
2018-07-29 00:44:07 -0300

    teste 2

--- aluno-certificado-palestra.php
+++ aluno-certificado-palestra.php
@@ -119,7 +119,7 @@ try {
     $mail->Subject = 'Here is the subject';
     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-	//$mail->addAttachment($output,  'certificado.pdf', 'base64', 'application/pdf', 'attachment');
+	$mail->addAttachment($output,'application/pdf','certificado.pdf', false);
     $mail->send();
     echo 'Message has been sent';
 } catch (Exception $e) {
