19dc53e832bec677807a1016d4ee45ac62f253ce
gregoryjvrusso <gregoryjvrusso@gmail.com>
2018-07-29 00:09:41 -0300

    teste 2

--- aluno-certificado-palestra.php
+++ aluno-certificado-palestra.php
@@ -102,3 +102,27 @@ $dompdf->stream(
     )
 );
 
+$mail = new PHPMailer;
+try {
+    $mail->SMTPDebug = 2;                              
+    $mail->isSMTP();                            
+	$mail->Host = 'smtp.gmail.com';
+	$mail->Port = 587;
+    $mail->SMTPSecure = 'tls';                            
+    $mail->SMTPAuth = true;                               
+    $mail->Username = 'gregoryjvrusso@gmail.com';                
+    $mail->Password = 'savoia1914';                           
+    //Recipients
+    $mail->setFrom('gregoryjvrusso@gmail.com', 'Mailer');
+    $mail->addAddress('gregory_savoia@gmail.com', 'Joe User');     
+
+    $mail->isHTML(true);                                  // Set email format to 
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
