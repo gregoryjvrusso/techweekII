71cf6e4853ab7de2daf6a6b75ce502216b15c406
gregoryjvrusso <gregoryjvrusso@gmail.com>
2018-07-28 23:57:31 -0300

    teste 2

--- aluno-certificado-palestra.php
+++ aluno-certificado-palestra.php
@@ -1,4 +1,7 @@
 <?php
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
 require_once("dompdff/dompdf_config.inc.php");
 require_once("banco-aluno.php");
 require_once("banco-palestra.php");
@@ -101,23 +104,42 @@ $dompdf->stream(
     )
 );
 
-$mail = new PHPMailer;
-$mail->IsSMTP(true); // Define que a mensagem será SMTP
-$mail->Host = "smtp.gmail.com"; // Endereço do servidor SMTP
-$mail->Port = 587;
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'ssl';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-$mail->setFrom('gregoryjvrusso@gmail.com', 'Your Name');
-$mail->addAddress('gregoryjvrusso@gmail.com', 'My Friend');
-$mail->SMTPDebug  = 1; 
-$mail->Subject = 'An HTML Message';
-$mail->isHTML(true);
-$mail->Body = 'Hello, <b>my friend</b>! This message uses HTML!';
-var_dump($mail);
-if(!$mail->send()) {
-  echo 'Message was not sent.';
-  echo 'Mailer error: ' . $mail->ErrorInfo;
-} else {
-  echo 'Message has been sent.';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'user@example.com';                 // SMTP username
+    $mail->Password = 'secret';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+    $mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
 }
